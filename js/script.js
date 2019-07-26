8a95100aa3493a1e47050a825eb6168b510bb5b6
HectorCo <hcompanyortega@gmail.com>
2019-07-26 16:14:06 +0200

    añado efectos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-$(document).ready(function() {
-    jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000  // 2 seconds
+$(window).scroll(function() {
+  $('#textoAnimado').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+600) {
+      $(this).addClass("slideLeft");
+    }
   });
+});
+
+
+
+
+$(window).scroll(function() {
+  $('#entradaVideo').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+900) {
+      $(this).addClass("fadeIn");
+    }
+  });
+});
 
 
